3219f2ce9c82462f66694478f80cae62d52aaf73
tobalan <127816009+tobalan@users.noreply.github.com>
2023-06-18 23:43:15 +0530

    CRC-001 try no proxy for epg url

--- epg.py
+++ epg.py
@@ -25,7 +25,7 @@ def genEPG(i, c):
     global channel, programme, error, result, API, IMG, done
     # for day in range(-7, 8):
     # 1 day future , today and two days past to play catchup
-    for day in range(-2, 2):
+    for day in range(-1, 0):
         try:
             resp = requests.get(f"{API}/v1.3/getepg/get", params={"offset": day,
                                 "channel_id": c['channel_id'], "langId": "6"}).json()
